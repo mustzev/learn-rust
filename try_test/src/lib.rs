026e67f341c29d6bf438669acdec32bd24159a0a
Sukhbold <sukhbold.a@qpay.mn>
2024-02-19 19:22:02 +0800

    .

--- try_test/src/lib.rs
+++ try_test/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -15,5 +23,19 @@ mod tests {
     #[test]
     fn it_fails() {
         panic!("It will panic!")
+    }
+
+    #[test]
+    fn it_fails_result() -> Result<(), String> {
+        if 2 + 2 == 6 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal six"))
+        }
+    }
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
---
+++ try_test/tests/integration_test.rs
@@ -0,0 +1,6 @@
+use try_test;
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, try_test::add_two(2));
+}
