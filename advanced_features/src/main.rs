870ffaa411f1f051dc311d57e666d7bebdf5fba4
Sukhbold <sukhbold.a@qpay.mn>
2024-07-25 18:33:28 +0800

    .

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -1,5 +1,6 @@
 mod unsafe_rust;
 
 fn main() {
-    unsafe_rust::unsafe_rust::make_unsafe_rust();
+    // unsafe_rust::unsafe_rust::make_unsafe_rust();
+    unsafe_rust::unsafe_rust::split_at_mut(&mut [10, 11, 12], 3);
 }
