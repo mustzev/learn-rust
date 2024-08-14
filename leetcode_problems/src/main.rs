7d6cd3f6dcfb5bca2ff89cba569a4ac6ad3622d4
Sukhbold <sukhbold.a@qpay.mn>
2024-08-14 16:26:46 +0800

    .

--- leetcode_problems/src/main.rs
+++ leetcode_problems/src/main.rs
@@ -1,16 +1,5 @@
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for i in 0..nums.len() {
-        for j in 0..nums.len() {
-            if i != j {
-                if (nums[i] + nums[j]) == target {
-                    return vec![i.try_into().unwrap(), j.try_into().unwrap()];
-                }
-            }
-        }
-    }
-    return vec![-1, -1];
-}
+mod problems;
 
 fn main() {
-    two_sum(vec![3, 2, 4], 6);
+    problems::add_two_numbers_2::test_add_two_numbers();
 }
